b7aac90836381cf627da9a0bbdd6e99a4b52f43c
tachen <tatoschka1234@users.noreply.github.com>
2025-05-14 23:09:57 -0700

    event rep

--- src/main/java/ru/yandex/practicum/filmorate/model/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.filmorate.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.model.enums.EventOperation;
+import ru.yandex.practicum.filmorate.model.enums.EventType;
 
 @Data
 @NoArgsConstructor
@@ -11,7 +13,7 @@ public class Event {
     private Long eventId;
     private Long timestamp;
     private Long userId;
-    private String eventType;
-    private String operation;
+    private EventType eventType;
+    private EventOperation operation;
     private Long entityId;
 }
